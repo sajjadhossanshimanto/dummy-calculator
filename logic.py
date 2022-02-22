3d037f2ee2463c7da422f8f7aa913e3be5468f13
shimanto <s.m.sajjadhossanshimanto.c@gmail.com>
2022-02-22 09:53:35 +0000

    moved inside class

--- logic.py
+++ logic.py
@@ -19,45 +19,51 @@ FloatPart = TypeVar('FloatPart')# str
 IntPart = TypeVar('IntPart', int, str)
 
 
-def operator(n)-> str:
-    return ope[base](int(n))[2:]
+class Converter:
+    base=2
 
-def positional_mul(number:FloatPart, base:int)-> Tuple[IntPart, FloatPart]:
-    # assert isinstance(number, str)
+    def __new__(cls, number):
+        return
 
-    pre_len = len(number)
-    number = str(int(number)*base)
-    number.rstrip('0')
-    cur_len = len(number)
+    def operator(self, n)-> str:
+        return ope[base](int(n))[2:]
 
-    start = cur_len-pre_len
-    return number[:start] or 0, number[start:] or '0'
+    def positional_mul(self, number:FloatPart)-> Tuple[IntPart, FloatPart]:
+        # assert isinstance(number, str)
 
-def fmod(n:str) -> Tuple[IntPart, FloatPart]:
-    # assert isinstance(n, float), 'type error'
+        pre_len = len(number)
+        number = str(int(number)*base)
+        number.rstrip('0')
+        cur_len = len(number)
 
-    _int, _, _float = str(n).strip('0').partition('.')
-    return _int or 0, _float or '0'
+        start = cur_len-pre_len
+        return number[:start] or 0, number[start:] or '0'
 
-def float_to_bin(n:FloatPart) -> str:
-    res='.'
-    while True:
-        if len(res)>=(max_round+1):
-            return res+'...'
+    def fmod(self, n:str) -> Tuple[IntPart, FloatPart]:
+        # assert isinstance(n, float), 'type error'
 
-        int_part, n = positional_mul(n, base)
-        res+=operator(int_part)
-        if int(n)==0:
-            break
+        _int, _, _float = str(n).strip('0').partition('.')
+        return _int or 0, _float or '0'
 
-    return res
+    def float_to_bin(self, n:FloatPart) -> str:
+        res='.'
+        while True:
+            if len(res)>=(max_round+1):
+                return res+'...'
 
-def dec_to_bin(n:str) -> str:
-    if '.' not in n:# non-floating point
-        return operator(n)
-    
-    _int, _float = fmod(n)
-    return operator(_int) + float_to_bin(_float)          
+            int_part, n = self.positional_mul(n)
+            res += self.operator(int_part)
+            if int(n)==0:
+                break
+
+        return res
+
+    def dec_to_bin(self, n:str) -> str:
+        if '.' not in n:# non-floating point
+            return self.operator(n)
+        
+        _int, _float = self.fmod(n)
+        return self.operator(_int) + self.float_to_bin(_float)          
 
 
 if __name__=='__main__':
